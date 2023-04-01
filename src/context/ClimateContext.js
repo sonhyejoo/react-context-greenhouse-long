77db918fcae37c4c6b1a8cfe1b4aabb89cc12d95
sonhyejoo <seongwoo.cho@gmail.com>
2023-04-01 15:30:40 -0400

    climateContext and provider

--- src/context/ClimateContext.js
+++ src/context/ClimateContext.js
@@ -1,2 +1,23 @@
+import { createContext, useContext, useState } from "react";
+
 // Temperature has a default value of 50 degrees
 // Humidity has a default value of 40%
+export const ClimateContext = createContext();
+export const useClimate = () => useContext(ClimateContext);
+
+export default function ClimateProvider({ children }) {
+  const [temp, setTemp] = useState(50);
+  const [humid, setHumid] = useState(0.4);
+  return (
+    <ClimateContext.Provider
+      value={{
+        temp,
+        setTemp,
+        humid,
+        setHumid,
+      }}
+    >
+      {children}
+    </ClimateContext.Provider>
+  );
+}
