db9f3f16bea4b1f2b07c89548dede62c141b7885
sonhyejoo <seongwoo.cho@gmail.com>
2023-04-01 15:22:49 -0400

    lightswitch

--- src/components/Greenhouse/LightSwitch.js
+++ src/components/Greenhouse/LightSwitch.js
@@ -1,10 +1,26 @@
-import './LightSwitch.css';
+import "./LightSwitch.css";
+import { useTheme } from "../../context/ThemeContext";
 
 function LightSwitch() {
+  const { themeName, setThemeName } = useTheme();
+
+  function handleClick(onOff) {
+    if (onOff === "on") {
+      setThemeName("day");
+      return;
+    } else if (onOff === "off") {
+      setThemeName("night");
+      return;
+    }
+  }
   return (
-    <div className="light-switch day">
-      <div className="on">DAY</div>
-      <div className="off">NIGHT</div>
+    <div className={`light-switch ${themeName}`}>
+      <div className="on" onClick={() => handleClick("on")}>
+        DAY
+      </div>
+      <div className="off" onClick={() => handleClick("off")}>
+        NIGHT
+      </div>
     </div>
   );
 }
